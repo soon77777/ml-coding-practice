b6c38c0dc75f6492c1fa3ce8ebe3f2582be0b900
soon77777 <a071560@gmail.com>
2026-03-15 21:09:32 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -345,3 +345,14 @@ print(ord('0'))
 
 # 객체 x가 가진 멤버 변수와 멤버 함수 보여주기
 print(dir([1,2,3]))
+print(dir({'1':'a'}))
+print(dir(1))
+
+print(int('3'))
+print(str(3))
+
+# x를 리스트로 반환
+print(list("Python"))
+print(1,2,3)
+
+# 
