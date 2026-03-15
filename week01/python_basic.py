335e302b07987197407a6d4d0ceca5b998f47a8a
soon77777 <a071560@gmail.com>
2026-03-15 20:58:31 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -280,3 +280,13 @@ else:
     print('저금하자!')
 
 """# 4. 반복문"""
+
+# for 반복문1
+test_list = ['one','two','three']
+for i in test_list:
+    x = i + '!'
+    print(x)
+
+# for 반복문2
+number = 0
+for score in [90,25,67,45,93]:
