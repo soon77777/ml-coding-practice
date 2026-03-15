40d594a51b48b223d711e6dc6277c02eb9b9609c
soon77777 <a071560@gmail.com>
2026-03-15 20:46:31 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -103,3 +103,36 @@ print(d[0:3])
 print(a+b)
 print(b[0] + "hi~ ^^;")
 # print(a[0] + " hi~ ^^;") #오류 발생
+
+# 리스트 반복
+print(a * 3)
+
+#리스트 수정
+a[2] = 99
+print(a)
+
+a[1:2] = ['a', 'b', 'c']
+print(a)
+
+a[-1] = ['d', 'e', 'f']
+print(a)
+
+# 삭제
+del a[-1]
+print(a)
+
+# 원소 추가
+a.append(5)
+print(a)
+
+# 원소 정렬
+b.sort()
+print(a)
+
+# 원소 순서 뒤집기
+a = [3, 4, 1, 9]
+a.reverse()
+print(a)
+
+# 원소 위치 확인
+print(a.index(9))
