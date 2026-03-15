9a760438d6949b5d470459ef4211de71ffffb8f5
soon77777 <a071560@gmail.com>
2026-03-15 21:03:31 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -311,3 +311,20 @@ def sum1(a,b):
 def sum2(*args):
     x= 0
     for i in args:
+        x += i
+    return x
+
+# 함수 호출
+a = 5
+b = 3
+print(sum1(a,b))
+print(sum1(3,5))
+print(sum2(1,2,3,4,5))
+print(sum2(2, 3.5,10))
+
+"""## 내장 함수"""
+
+# 숫자 x의 절대값을 반환
+print(abs(-3.5))
+
+#그룹 자료형의 변수 x의 모든 원소가 참(0이 아닌 값)이면 True 반환
