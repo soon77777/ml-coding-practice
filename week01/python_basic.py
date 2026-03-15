d4ccf28c65757149aa1114615c788e6615e22f42
soon77777 <a071560@gmail.com>
2026-03-15 21:18:32 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -434,4 +434,10 @@ for i in range(6,11):
     f.write(data)
 f.close()
 
-# 파일 모드
+# 파일 모드 'r' - readline()
+f = open('example.txt', 'r')
+
+
+while True:
+    line = f.readline()
+    if not line: break
