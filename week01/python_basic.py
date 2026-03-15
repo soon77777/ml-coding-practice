f9e4bc1cc07638848feadbf869329510cec76b11
soon77777 <a071560@gmail.com>
2026-03-15 21:14:02 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -391,3 +391,15 @@ print(range(5))
 print(list(range(5)))
 print(list(range(5,10)))
 print(list(range(5,10,2)))
+
+for i in range(5, 20, 3):
+    print(i)
+
+# 입력값 s의 길이를 반환
+len('Python')
+
+print(sorted([3,0,2,1]))
+print(sorted('Python'))
+
+# 패키지, 모듈 사용
+# Request('http://www.sunmoon.ac.kr')
