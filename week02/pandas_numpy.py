6c3609ce9827ee5688630c45f4a2b970240ae8ec
soon77777 <a071560@gmail.com>
2026-03-22 12:22:45 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -97,4 +97,4 @@ print(sample_df.describe())
 print(sample_df.std())
 print(sample_df.agg(['count','mean','std','min','max']))
 
-file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_'
+file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/iris.csv'
