907ddb6b9fc3566dee58558c7598f005d545e080
soon77777 <a071560@gmail.com>
2026-03-22 12:14:45 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -63,3 +63,9 @@ print(more2015_tv.head())
 
 more2015_or_tv = netflix[(netflix['release_year'] > 2015) | (netflix['type'] == 'TV Show')]
 print(more2015_or_tv.head())
+
+data = {
+    'name' : ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Frace',]
+
+
+}
