f40b071ce451195f850cdae64018edbf00fe6ed4
soon77777 <a071560@gmail.com>
2026-03-22 11:44:26 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-file_url = 'https://media.githubusercontent'
+file_url = 'https://media.githubusercontent.com/media/mustha'
 sample = pd.read_csv(file_url)
 
 print(sample.head())
