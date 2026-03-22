44d692d28905f0124d0e2481afefd8dcf242a4df
soon77777 <a071560@gmail.com>
2026-03-22 12:17:15 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -66,7 +66,12 @@ print(more2015_or_tv.head())
 
 data = {
     'name' : ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace','Hannah'],
-    
-
-
+    'comment_length' : [150, 200, 50, 300, 120, 180, 75, 160],
+    'likes' : [25,30,10,45,20,35,5,28],
+    'is_spam' : [False, False, True, False, False, True, False, False],
+    'has_image' : [True, False, True, True, False, False, True, True]
 }
+
+df = pd.DataFrame(data)
+print(df.head())
+
