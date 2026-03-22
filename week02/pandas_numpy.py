fc26f39a9190928690020f3fd28a4e0b432787a5
soon77777 <a071560@gmail.com>
2026-03-22 12:03:26 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -47,4 +47,9 @@ netflix = pd.read_csv('2.1.1.netflix.csv')
 print(netflix.head())
 
 print(netflix['release_year'])
-print(netflix['release_year'])
+print(netflix['release_year'] > 2015)
+
+more2015 = netflix[netflix['release_year'] > 2015]
+print(more2015.head(10))
+
+print(~(netflix['release_year']))
