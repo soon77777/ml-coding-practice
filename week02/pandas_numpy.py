89a20ca3e7257495ead5c8810ecc346d90c79973
soon77777 <a071560@gmail.com>
2026-03-22 12:00:26 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -39,3 +39,6 @@ print(sample_df.iloc[0:2])
 print(sample_df.iloc[0:3])
 print(sample_df.iloc[0:3, 2:4])       # 컬럼까지 동시에 인덱싱
 
+print(sample_df.drop(['var_1','var_3'],axis=1))    # 컬럼을 제거하려면 axis =
+print(sample_df.drop(['var_1','var_2'], axis=1))
+
