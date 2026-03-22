a2090b6d1e2c36edce965cb44194c16e3e7b7b07
soon77777 <a071560@gmail.com>
2026-03-22 11:44:56 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-file_url = 'https://media.githubusercontent.com/media/mustha'
+file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/sample.csv'
+
 sample = pd.read_csv(file_url)
 
 print(sample.head())
