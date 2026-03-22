32e39a7f90138dc3b8e9364f49d837697864ccf8
soon77777 <a071560@gmail.com>
2026-03-22 12:25:15 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -105,3 +105,5 @@ print(iris.head())
 print(iris.groupby('class').std())
 
 print(iris.drop('class', axis=1).agg(['sum','mean','std']))
+
+# print(iris.agg(['sum', 'mean'm 'std']))    # class 컬럼으로 인해 에러 발생
