da2879a806b43d99f34f9435e41441731100ba13
soon77777 <a071560@gmail.com>
2026-03-29 14:50:54 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -126,3 +126,9 @@ print(titanic.info())
 # 히스토그램 그리기
 plt.figure(figsize=(10,6))
 plt.hist(titanic['Age'], bins=20, color='seagreen', edgecolor='black')
+
+plt.xlabel('Age')
+plt.ylabel('count')
+plt.title('Distribution of Ages on the Titanic')
+plt.grid(axis='y', linestyle= '--', alpha =0.7)
+plt.savefig('Figure06.png')
