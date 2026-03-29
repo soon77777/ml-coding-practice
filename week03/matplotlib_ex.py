820cd01dc58dc99018f40dd8d42d3505b5846d4d
soon77777 <a071560@gmail.com>
2026-03-29 15:03:24 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -175,4 +175,5 @@ plt.fill_between(survived_counts.index.astype(str), survived_counts[1],
                  color='purple',alpha=0.9, label='Survived')
 
 # 나이대별 사망자
-plt.fill_between(survived_counts.index)
+plt.fill_between(survived_counts.index.astype(str), survived_counts[0],
+                 )
