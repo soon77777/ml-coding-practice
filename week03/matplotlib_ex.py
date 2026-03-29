b0a5d29e8833c39ffad270290407d5d33d1e6335
soon77777 <a071560@gmail.com>
2026-03-29 13:49:35 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -9,3 +9,11 @@ titanic = pd.read_csv('3.1.1.titanic.csv')
 
 # 데이터 처음 5개의 행 출력
 print(titanic.head())
+
+# 열에 대한 요약 정보 확인
+print(titanic.info())
+
+"""### **선 그래프 : 객실 등급에 따른 생존율 표시하기**"""
+
+# 객실 등급에 따른 생존자와 사망자의 평균 계산
+pclass_survived_mean = titanic.groupby('Pclass')
