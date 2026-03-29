01ebdafe8be9dfd45ebff38cc066d03fdf50d136
soon77777 <a071560@gmail.com>
2026-03-29 14:52:54 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -140,4 +140,4 @@ plt.close()
 titanic = titanic.dropna(subset=['Age','Fare'])
 
 # 상관 행렬 계산
-correlation_matrix = titanic.drop('PassengerId', axis=1)
+correlation_matrix = titanic.drop('PassengerId', axis=1).corr()
