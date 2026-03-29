00530a122a6dbe9e3e66164aa691b00c6043b9b5
soon77777 <a071560@gmail.com>
2026-03-29 14:36:24 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -77,3 +77,11 @@ for i, value in enumerate(survived_counts):
     plt.text(value + 1, i, str(value), ha='left',va='center')
 
 plt.savefig('Figure03.png')
+plt.close()
+
+"""## **산점도 그래프 : 나이와 요금, 생존 여부 확인하기**"""
+
+print(titanic.info(), '\n')
+
+# 결측치 처리
+titanic = titanic.dropna(subset=['Age','Fare','Survived'])
