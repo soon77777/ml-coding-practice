de9b0a2ccafc7f55e1d950c9fcb495505c1d3334
soon77777 <a071560@gmail.com>
2026-03-29 16:17:53 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -401,4 +401,11 @@ plt.close()
 ## **그래프 저장하기**
 
 # 히스토그램 그리기
-plt.hist(titanic['Age'], bins=20, color='skyblue', edgeco)
+plt.hist(titanic['Age'], bins=20, color='skyblue', edgecolor='black')
+plt.xlabel('Age')
+plt.ylabel('Count')
+plt.title('Distribution of Ages on the Titanic')
+plt.grid(axis='y', linestyle= '--', alpha=0.7)
+
+# 이미지로 저장
+plt.savefig('Figure17.png')
