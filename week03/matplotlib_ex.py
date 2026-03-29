5a938965412ffb9f0671e3fce1cda489ae3bcf14
soon77777 <a071560@gmail.com>
2026-03-29 15:11:54 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -208,4 +208,4 @@ plt.close()
 """### **바이올린 플롯 : 승객 등급에 따른 나이 분포 표시하기**"""
 
 # 결측치 처리
-titanic['Age'] = titanic['Age'].fillna(titanic)
+titanic['Age'] = titanic['Age'].fillna(titanic['Age'].mean())
