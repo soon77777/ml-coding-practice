3aece4981f675cb4d29303c53ce83c4035917267
soon77777 <a071560@gmail.com>
2026-03-29 16:06:23 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -370,3 +370,12 @@ print(parch_counts)
 
 # x, y값 설정
 x = parch_counts.index.astype(str) # 데이터프레임의 인덱스를 문자열로 변환
+y1 = parch_counts[0].values # 사망자
+y2 = parch_counts[1].values # 생존자
+
+# plt.subplots() 함수를 사용하여 그래프 생성
+# figure와 서브플롯 생성
+fig, ax1 = plt.subplots()
+
+# 첫 번째 그래프 설정 (선 그래프)
+ax.plot(x, y1, '-s', color='indigo', markersize=7, linew)
