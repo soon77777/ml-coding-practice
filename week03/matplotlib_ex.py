8fe1b2f06dd7b35e550ff9f9fb7dd5f1aed4052d
soon77777 <a071560@gmail.com>
2026-03-29 14:01:35 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -42,4 +42,7 @@ print(survived_counts)
 # 막대 그래프 그리기
 plt.bar(survived_counts.index, survived_counts,
         color = ['mediumorchid', 'darkviolet', 'indigo'])
-plt.title('Survived Counts by Emba')
+plt.title('Survived Counts by Embarked Port on Titanic')
+plt.xlabel('Embarked Port')
+plt.ylabel('Count')
+plt.xticks(survived_counts.index, ['Southampton', ])
