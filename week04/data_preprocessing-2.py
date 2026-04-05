a85adc9218e922c8bd0588c3ee58b74583a02c6f
soon77777 <a071560@gmail.com>
2026-04-05 18:11:12 +0900

    Updated Changes

--- week04/data_preprocessing-2.py
+++ week04/data_preprocessing-2.py
@@ -26,4 +26,9 @@ for set_ in (strat_train_set, strat_test_set):
 """
 
 housing = strat_train_set.drop("median_house_value", axis=1)
-housing_lab
+housing_labels =strat_train_set["median_house_value"].copy()
+
+# 데이터 정제
+# null 값이 있는 행 확인하기
+null_rows_idx = housing.innull().any(axis=1)
+housing.loc[null]
