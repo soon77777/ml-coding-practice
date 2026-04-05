924938ab4e7e59acffd807e441b9c63cd0049981
soon77777 <a071560@gmail.com>
2026-04-05 17:42:12 +0900

    Updated Changes

--- week04/data_preprocessing-2.py
+++ week04/data_preprocessing-2.py
@@ -13,4 +13,4 @@ housing["income_cat"] = pd.cut(housing["median_income"],
                                bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
                                labels=[1,2,3,4,5])
 
-strat_
+strat_train_seet, strat_test_set = train_test_split()
