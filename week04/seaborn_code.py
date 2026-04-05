b961da2209f287fc8d1e5590509145cb70b4789d
soon77777 <a071560@gmail.com>
2026-04-05 17:11:11 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -18,13 +18,13 @@ fig = plt.figure(figsize=(15,5))
 ax1 = fig.add_subplot(1,2,1)
 ax2= fig.add_subplot(1,2,2)
 
-# # stripplot( ) 그리기
-# sns.stripplot(x='day', y='tip', hue='sex', data=tips, alpha=0.7, ax=ax1)
+# stripplot( ) 그리기
+sns.stripplot(x='day', y='tip', hue='sex', data=tips, alpha=0.7, ax=ax1)
 
-# # swarmplot( ) 그리기
-# sns.swarmplot(x='day', y='tip',hue='sex', data=tips, palette='Set2', alpha=0.7,ax=ax2)
+# swarmplot( ) 그리기
+sns.swarmplot(x='day', y='tip',hue='sex', data=tips, palette='Set2', alpha=0.7,ax=ax2)
 
-# # 서브 플롯의 제목 설정
-# ax1.set_title('Strip Plot of Tip by Day and Gender')
-# ax2.set_title('Swarm Plot of Tip by Day and Gender')
-# plt.savefig('./week04/Seaborn_Figure01.jpg')
+# 서브 플롯의 제목 설정
+ax1.set_title('Strip Plot of Tip by Day and Gender')
+ax2.set_title('Swarm Plot of Tip by Day and Gender')
+plt.savefig('./week04/Seaborn_Figure01.jpg')
