f2b6018312b89ca8e38a075986988aad7aa6c2d0
soon77777 <a071560@gmail.com>
2026-04-05 18:06:12 +0900

    Updated Changes

--- week04/data_preprocessing-2.py
+++ week04/data_preprocessing-2.py
@@ -19,4 +19,8 @@ strat_train_seet, strat_test_set = train_test_split(
 for set_ in (strat_train_set, strat_test_set):
     set_.drop("income_cat", axis=1, inplace=True)
 
-""""""
+"""
+* 원본 훈련 세트로 복원하고 타깃을 분리
+* 'strat_train_set.drop()'은 지정한 열을 제외한 'strat_train_set'의 복사본을 만듦
+
+"""
