f3077e68f08f258968bbafc33e029ea6d73fec30
soon77777 <a071560@gmail.com>
2026-04-05 18:16:12 +0900

    Updated Changes

--- week04/data_preprocessing-2.py
+++ week04/data_preprocessing-2.py
@@ -40,3 +40,8 @@ imputer = SimpleImputer(strategy="median")
 # 수치형 특성만 추출
 housing_num = housing.select_dtypes(include=[np.number])
 housing_num.head()
+
+imputer.fit(housing_num)
+
+print(imputer.statistics_)      # imputer 결과 값
+print(housing_num.median())
