1a5ce345f5d0a66c3b549f6104c42d4c73bec10e
soon77777 <a071560@gmail.com>
2026-04-05 18:23:42 +0900

    Updated Changes

--- week04/data_preprocessing-2.py
+++ week04/data_preprocessing-2.py
@@ -65,3 +65,13 @@ outlier_pred
 
 housing = housing.iloc[outlier_pred == 1]
 housing_labels = housing_labels.iloc[outlier_pred == 1]
+
+# 텍스트와 범주형 특성 다루기
+housing_cat = housing[["ocean_proximity"]]
+housing_cat.head(8)
+
+from sklearn.preprocessing import OrdinalEncoder
+
+ordinal_encoder = OrdinalEncoder()
+housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
+
