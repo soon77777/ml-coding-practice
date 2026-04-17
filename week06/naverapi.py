662c3cf672fe714486abcab981eeb8b4d2860ae0
soon77777 <a071560@gmail.com>
2026-04-17 18:49:05 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -15,3 +15,6 @@ def main():
     jsonResult = []
 
     jsonResponse = getNaverSearch(nodde, srcText, 1, 100)     # [CODE 2]
+    total = jsonResponse['total']
+
+    while((jsonResponse != None)) and (jsonResponse['display'] != 0)
