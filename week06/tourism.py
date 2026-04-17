5fcb534f4dae5984107fa20bd6378e6b2a68c01a
soon77777 <a071560@gmail.com>
2026-04-17 19:35:36 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -50,4 +50,9 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
            ed = jsonData['response']['body']['items']['item']['ed']
            print('[ %s_%s : %s]' % (natName, yyyymm, num))
            print('----------------------------------------------------')
-           jsonResult.append({'nat_name': natName, 'nat_cd': nat_cd, 'yyyymm'})
+           jsonResult.append({'nat_name': natName, 'nat_cd': nat_cd, 'yyyymm': yyyymm, 'visit_cnt': num})
+           result.append([natName, nat_cd, yyyymm, num])
+
+    return (jsonResult, result, natName, ed)
+
+"""### [CODE 2]"""
