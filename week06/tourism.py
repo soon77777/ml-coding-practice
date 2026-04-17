983739742465501880f3228d09dabd2acb456546
soon77777 <a071560@gmail.com>
2026-04-17 19:29:05 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -35,3 +35,7 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
       for month in range(1, 13):
         yyyymm = "{0}{1:0>2}".format(str(year), str(month))
         jsonData = getTourismStatsItem(yyyymm, nat_cd, ed_cd)    #[CODE 2]
+        if (jsonData['response']['header']['resultMsg'] == 'OK'):
+           #데이터가 없는 마지막 항목인 경우 ----------------------
+           if jsonData['response']['body']['items'] == '':
+              dataEND = "{0}{1:0>2}".format(str(year), str(month-1))
