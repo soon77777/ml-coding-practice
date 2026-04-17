9140ade9657e4ae543a98628d279c6400e417574
soon77777 <a071560@gmail.com>
2026-04-17 18:42:05 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+import urllib.request
+import datetime
+import json
+
+client_id = 'Client ID'
+client_secret = 'Client Secret'
+
+def main():
+
+    node = 'news'
+    srcTeext = input('검색어를 입력하세요: ')
+
+    cnt = 0
+    jsonResult = []
+
+    jsonResponse = getNaverSearch(nodde, srcText, 1, 100)     # 
