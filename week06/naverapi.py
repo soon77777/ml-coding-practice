9156de6c4562efa5e5d0e6fa95c8a536685b978e
soon77777 <a071560@gmail.com>
2026-04-17 18:54:35 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -31,3 +31,7 @@ def main():
             jsonFile = json.dumps(jsonResult, indent = 4, sort_keys = True,
                                   ensure_ascii = False)
             
+            outfile.write(jsonfile)
+
+        print("가져온 데이터 : %d 건" %(cnt))
+        print('%s_naver_%s.josn SAVED' % (srcText, node))
