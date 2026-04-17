f775304ef265c82519f76318856a6380bef8609d
soon77777 <a071560@gmail.com>
2026-04-17 23:10:27 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -69,7 +69,7 @@ def getRequestUrl(url):
 
 
 def getPostData(post, jsonResult, cnt):  #[CODE 3]
-    title = post('title')
+    title = post['title']
     description = post['description']
     org_link = post['originallink']
     link = post['link']
