72c1a4d2bb746c65954c45892edbf30afd60e2ae
soon77777 <a071560@gmail.com>
2026-05-05 08:32:16 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -26,4 +26,8 @@ print(y[0])        # 샘플 데이터 레이블 확인 (=5)
 
 # 10x10 그림 생성
 plt.figure(figsize=(9, 9))
-for idx, image_data in enumerate
+for idx, image_data in enumerate(X[:100]):
+    plt.subplot(10, 10, idx + 1)
+    plot_digit(image_data)
+plt.subplots_adjust(wspace=0, hspace=0)
+    plt.show()
