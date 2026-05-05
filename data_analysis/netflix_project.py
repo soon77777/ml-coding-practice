a5d893c9bf16c735606cfd050630d2fae0e6184e
soon77777 <a071560@gmail.com>
2026-05-05 21:01:48 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -87,4 +87,7 @@ import seaborn as sns
 netflix = pd.read_csv('netflix_preproceessed.csv')
 
 # 넷플릭스 브랜드 상징 색깔 시각화
-sns.palplot(['#221f1f', '#b20710', '#e50914', '#f5f5f1'])
+sns.palplot(['#221f1f', '#b20710', '#e50914','#f5f5f1'])
+
+# 제목 정하기
+plt.title('Netflix brand palette', loc='left', fontfamily='serif',)
