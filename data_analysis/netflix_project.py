7c28dd5594dad6076c9dc0c82f706f50a9de086e
soon77777 <a071560@gmail.com>
2026-05-05 18:09:50 +0900

    commit

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+# 넷플릭스 데이터 분석 프로젝트
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#세션 저장소에 업로드한 csv 파일을 읽어 변수에 할당
+netflix = pd.read_csv('netflix_titles.csv')
+netflix.head()
