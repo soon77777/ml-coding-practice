670d8af55619a0fb675dfb8f190a4b57d1f917f3
soon77777 <a071560@gmail.com>
2026-05-05 09:34:47 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -70,4 +70,10 @@ cm = confusion_matrix(y_train_5, y_train_pred)
 print(cm)
 
 y_train-perfect_predictions = y_train_5  # 완벽한 분류기일 경우
-print(confusion_matrix(y_train_5, y_train_per))
+print(confusion_matrix(y_train_5, y_train_perfect_predictions))
+
+# 정밀도와 재현율
+from sklearn.metrics import precision_score, recall_score
+
+print(precision_score(y_train_5, y_train_pred))  # == 3530 / (687 + 3530)
+
