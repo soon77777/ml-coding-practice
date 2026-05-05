d6b4ed9c2a5395a8111efb4356173586ff160574
soon77777 <a071560@gmail.com>
2026-05-05 20:42:06 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -40,4 +40,7 @@ netflix['cast'] = netflix['cast'].replace(np.nan, 'No Data')
 netflix.dropna(axis = 0, inplace = True)
 
 # .info() : 열에 대한 요약 정보 확인
-# 8807 rows(원본 데이터 행 개수) - 17 rows(결측치 행) = 8790 rows(결측치)
+# 8807 rows(원본 데이터 행 개수) - 17 rows(결측치 행) = 8790 rows(결측치가 제거된 행 개수)
+netflix.info()
+
+# 데이터프레임의 각
