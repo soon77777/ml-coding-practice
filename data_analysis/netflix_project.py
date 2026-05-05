c5114e87aa47b219ce6f60ae794243b01231b46b
soon77777 <a071560@gmail.com>
2026-05-05 15:47:29 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
  # 넷플릭스 데이터 분석 프로젝트
 
-# import numpy as np
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# # 세션 저장소에 업로드한 csv 파일을 읽어 변수에 할당
-# netflix = pd.read_csv('netflix_titles.csv')
-# netflix.head()
-import os
-print(os.getcwd())  # 현재 파이썬이 실행되고 있는 폴더 위치 출력
+# 세션 저장소에 업로드한 csv 파일을 읽어 변수에 할당
+netflix = pd.read_csv('netflix_titles.csv')
+netflix.head()
