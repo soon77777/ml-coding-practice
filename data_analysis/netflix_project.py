aee233093036190823f09e9732944a580d9d01d6
soon77777 <a071560@gmail.com>
2026-05-05 21:40:19 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -186,4 +186,5 @@ cmap = plt.matplotlib.colors.LinearSegmentedColormap.from_list('',['#221f1f', '#
 sns.heatmap(netflix_age_country_unstack, cmap = cmap, linewidth=2.5, annot=True, fmt='.0%')
 
 plt.suptitle('Target ages proportion of total content by country',
-             f)
+             fontweight='bold', fontfamily='serif', fontsize=15)
+plt.title('Here we see interesting differences between countries. Most shows')
