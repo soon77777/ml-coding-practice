cda61190324ddd658a7852ede8793ab40dc12ade
soon77777 <a071560@gmail.com>
2026-05-05 20:31:35 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -23,4 +23,4 @@ netflix.info()
 for i in netflix.columns :
     missingValueRate = netflix[i].isna().sum() / len(netflix) * 100
     if missingValueRate > 0 :
-        print("{} null rate: {}%")
+        print("{} null rate: {}%".format(i,round))
