aef899d78eceac6a2d04b27a24b0db09518a07f6
soon77777 <a071560@gmail.com>
2026-05-05 21:40:49 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -187,4 +187,4 @@ sns.heatmap(netflix_age_country_unstack, cmap = cmap, linewidth=2.5, annot=True,
 
 plt.suptitle('Target ages proportion of total content by country',
              fontweight='bold', fontfamily='serif', fontsize=15)
-plt.title('Here we see interesting differences between countries. Most shows')
+plt.title('Here we see interesting differences between countries. Most shows in South Korea are')
