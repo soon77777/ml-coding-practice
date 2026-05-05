398a84d644d46d063d51b720284fa61b6a11ef92
soon77777 <a071560@gmail.com>
2026-05-05 21:10:19 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -108,4 +108,10 @@ plt.pie(type_counts, labels=type_counts.index, autopct='%0.f%%', startangle=100,
         explode=[0.05, 0.05], shadow=True, colors=['#b20710', '#221f1f'])
 
 plt.suptitle('Movie & TV Show distribution', fontfamily='serif', fontsize=15, fontweight='bold')
-plt.title('We see more movies than TV shows on Netflix.', fontfamily='serif')
+plt.title('We see more movies than TV shows on Netflix.', fontfamily='serif', fontsize=12)
+plt.show()
+
+netflix.head(3)
+
+# 넷플릭스 데이터셋의 장르별 등장 횟수 계산
+genres
