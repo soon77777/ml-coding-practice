b1b10850111d6806e237519f06a8f8188871d526
soon77777 <a071560@gmail.com>
2026-05-05 09:48:47 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -80,4 +80,12 @@ print(precision_score(y_train_5, y_train_pred))  # == 3530 / (687 + 3530)
 print(recall_score(y_train_5, y_train_pred))  # == 3530 / (1891 + 3530)
 
 y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3,
-                             method="deci")
+                             method="decision_function")
+roc_auc_score(y_train_5, y_scores)
+
+#다음은 실행하는데 몇 분이 걸릴 수 있음
+from sklearn.ensemble import RandomForestClassifier
+
+forest_clf = RandomForestClassifier(random_state=42)
+
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,)
