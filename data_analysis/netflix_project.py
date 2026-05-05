50957065157af4b5f2da44d11483c0f6b974ff6a
soon77777 <a071560@gmail.com>
2026-05-05 20:30:05 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -12,3 +12,13 @@ netflix.head()
 
 # .columns : 열 이름 확인
 list(netflix.columns)
+
+# .head(3) : 데이터 처음 3개의 행 출력
+netflix.head(3)
+
+# .info() : 열에 대한 요약 정보 확인
+netflix.info()
+
+# 넷플릭스 결측치 비율 확인하기
+for i in netflix.columns :
+    missingValueRate = netflix[i].isna().sum() /
