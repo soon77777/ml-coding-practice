ba96a0b39d5be314c325633b95f5767b91ef8939
soon77777 <a071560@gmail.com>
2026-05-15 14:04:46 +0900

    Updated Changes

---
+++ week08/svm.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# 선형 SVM 분류
+
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
