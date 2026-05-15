594c5bad34086e42f43fe8c0144180c401c7351c
soon77777 <a071560@gmail.com>
2026-05-15 14:08:16 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -11,4 +11,11 @@ iris = load_iris(as_frame=True)
 X = iris.data[["petal length (cm)", "petal width (cm)"]].values
 y = (iris.target == 2)  # Iris virginica
 
-svm_clf = make_pipeline(StandardScaler(),LinearSVC(C=1, dual=True, random_state=42))
+svm_clf = make_pipeline(StandardScaler(),
+                        LinearSVC(C=1, dual=True, random_state=42))
+svm_clf.fit(X,y)
+
+X_new = [[5.5, 1.7], [5.0, 1.5]]
+
+print(svm_clf.predict(X_new))
+print(svm_clf.decision_function(X_new))
