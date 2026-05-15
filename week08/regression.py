e16bb7e977b2f05c1c234d39b4c7191f3bb792d6
soon77777 <a071560@gmail.com>
2026-05-15 14:02:16 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -165,5 +165,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 softmax_reg = LogisticRegression(C=30, random_state=42)
 softmax_reg.fit(X_train, y_train)
 
-print(softmax_reg.predict([5,2]))
-print(softmax_reg.predict_proba([5,2]).round(2))
+print(softmax_reg.predict([[5,2]]))
+print(softmax_reg.predict_proba([[5,2]]).round(2))
