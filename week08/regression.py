a15388cf6dc26c38c99eede2dc4b066f665879b0
soon77777 <a071560@gmail.com>
2026-05-15 13:53:46 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -106,7 +106,7 @@ ridge_reg = Ridge(alpha=0.1, solver="cholesky")
 ridge_reg.fit(X, y)
 print(ridge_reg.predict([[1.5]]))
 
-sgd_reg = SGDRegressor(penalty="12", alpha=0.1 / m, tol=None,
+sgd_reg = SGDRegressor(penalty="l2", alpha=0.1 / m, tol=None,
                        max_iter=1000, eta0=0.01, random_state=42)
 sgd_reg.fit(X, y.ravel())  # fit()은 1D 타겟을 기대하므로 y.ravel()을 사용합니다.
 print(sgd_reg.predict([[1.5]]))
