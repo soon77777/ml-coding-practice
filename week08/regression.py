15804f59e171470721869038f1d7956559d238bc
soon77777 <a071560@gmail.com>
2026-05-15 13:46:16 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -30,7 +30,7 @@ lin_reg = LinearRegression()
 lin_reg.fit(X, y)
 lin_reg.intercept_, lin_reg.coef_
 
-X_new = np.array([0], [2])
+X_new = np.array([[0], [2]])
 print(lin_reg.predict(X_new))
 
 # 확률적 경사 하강법
