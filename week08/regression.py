84b6abadf64cc00a7adb4937b763573b7c3e7744
soon77777 <a071560@gmail.com>
2026-05-15 13:56:46 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -148,7 +148,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 x = iris.data[["petal width (cm)"]].values
-y = iris.targe_names[iris.target] == 'virginica'
+y = iris.target_names[iris.target] == 'virginica'
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
 log_reg = LogisticRegression(random_state=42)
