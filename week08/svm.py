cec9d1b01e807c3e145fdf86fb709deb53d5cf9e
soon77777 <a071560@gmail.com>
2026-05-15 14:21:46 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -45,3 +45,16 @@ poly_kernel_svm_clf.fit(X,y)
 # 가우스 RBF 커널
 rbf_kernel_svm_clf = make_pipeline(StandarScaler(),
                                    SVC(kernel="rbf", gamma=5, C=0.001))
+rbf_kernel_svm_clf.fit(X,y)
+
+# SVM 회귀
+
+from sklearn.svm import LinearSVR
+
+# 이 세 줄은 간단한 선형 데이터셋을 생성합니다.
+np.random.seed(42)
+X = 2 * np. random.rand(50, 1)
+y = 4 + 3 * X[:,0] + np.random.randn(50)
+
+svm_reg = make_pipeline(StandardScaler(),
+                        LinearSVR(epsilon=0.5, dual=True, random_state=42))
