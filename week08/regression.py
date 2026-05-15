4996ec03b247d89c4b9e7e89ebf06ba6f39f33c1
soon77777 <a071560@gmail.com>
2026-05-15 12:46:15 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -39,4 +39,4 @@ from sklearn.linear_model import SGDRegressor
 
 sgd_reg = SGDRegressor(max_iter=1000, tol=1e-5, penalty=None, eta0=0.01,
                        n_iter_no_change=100, random_state=42)
-sgd_reg.fit(X)
+sgd_reg.fit(X, y.reavel()) # fit()이 1D 타깃을 기대하기 때문에 y.ravel()로 씁니다
