897c5be54b2db7cc6e8d5ac76e303d0988ef66a0
soon77777 <a071560@gmail.com>
2026-05-25 20:00:02 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -7,4 +7,9 @@ y_iris = iris.target
 
 tree_clf = DecisionTreeClassifier(max_depth=2, random_state=42)
 tree_clf.fit(X_iris,y_iris)
-print(iris.)
+
+from sklearn.tree import export_graphviz
+
+export_graphviz(
+    tree_clf,
+)
