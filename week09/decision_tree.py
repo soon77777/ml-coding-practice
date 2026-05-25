36bd8f9334791b53c4464c02c975c0b5343af769
soon77777 <a071560@gmail.com>
2026-05-25 20:35:03 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -38,3 +38,5 @@ tree_clf1 = DecisionTreeClassifier(random_state=42)
 tree_clf2 = DecisionTreeClassifier(min_samples_leaf=5, random_state=42)
 tree_clf1.fit(X_moons, y_moons)
 tree_clf2.fit(X_moons, y_moons)
+
+X_moon
