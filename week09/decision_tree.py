a9e591a1d6a3044f659b15d0b9b623e3c35a4393
soon77777 <a071560@gmail.com>
2026-05-25 20:01:03 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -13,5 +13,8 @@ from sklearn.tree import export_graphviz
 export_graphviz(
     tree_clf,
     out_file="iris_tree.dot",
-    feature_names=["petal length (cm)", "petal width (cm)"]
+    feature_names=["petal length (cm)", "petal width (cm)"],
+    class_names=iris.target_names,
+    rounded=True,
+    filled=True
 )
